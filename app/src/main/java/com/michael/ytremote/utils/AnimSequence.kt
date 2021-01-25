ab45495a529840b85f89cd53d0aa645113b22ec0
toyota-m2k <toyota.mitsuki@gmail.com>
2021-01-26 00:08:05 +0900

    AnimSequenceクラスを試作

--- app/src/main/java/com/michael/ytremote/utils/AnimSequence.kt
+++ app/src/main/java/com/michael/ytremote/utils/AnimSequence.kt
@@ -4,88 +4,186 @@ package com.michael.ytremote.utils
 
 import android.animation.ValueAnimator
 import android.view.View
+import androidx.core.animation.doOnCancel
+import androidx.core.animation.doOnEnd
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 interface IAnimChip {
-    val precess:(Float)->Unit
-    val before:((reverse:Boolean)->Unit)?
-    val after:((reverse:Boolean)->Unit)?
+    fun process(r:Float)
+    fun before(reverse:Boolean)
+    fun after(reverse:Boolean)
 }
 
-abstract class AnimChipBase (
-    val start:Float,
-    val end:Float,
-    override val precess: (Float) -> Unit) : IAnimChip {
-
-    override var before: ((reverse: Boolean) -> Unit)? = null
-    override var after: ((reverse: Boolean) -> Unit)? = null
+interface IAnimEngine {
+    fun animate(reverse: Boolean, completed:((Boolean)->Unit)?)
+    fun cancel()
 }
 
-class ViewSizeAnimChip<O>(target:O, startDp:Int, endDp:Int, height:Boolean)
-    : AnimChipBase(startDp.toFloat(), endDp.toFloat(), {
-    if (height) {
-        target.setLayoutHeight(target.context.dp2px(it).toInt())
-    } else {
-        target.setLayoutWidth(target.context.dp2px(it).toInt())
+abstract class AbstractAnimChip : IAnimChip {
+    override fun before(reverse: Boolean) {
     }
-}) where O : View
+
+    override fun after(reverse: Boolean) {
+    }
+}
+
+class ViewSizeAnimChip<O>(private val target:O, private val startDp:Int, private val endDp:Int, val height:Boolean)
+    : AbstractAnimChip() where O : View {
+    override fun process(r: Float) {
+        if (height) {
+            target.setLayoutHeight(target.context.dp2px(startDp + r * (endDp - startDp)).toInt())
+        } else {
+            target.setLayoutWidth(target.context.dp2px(startDp + r * (endDp - startDp)).toInt())
+        }
+    }
+}
 
 
 
-class ViewVisibilityAnimationChip<O>(target:O, startVisible:Boolean, private val endVisible:Boolean, gone:Boolean=false, maxAlpha:Float=1f)
-    : AnimChipBase(bool2float(startVisible)*maxAlpha,bool2float(endVisible)*maxAlpha, {
-    target.alpha = it
-}) where O : View {
-    init {
-        before = { reverse->
-            val s = if(!reverse) startVisible else endVisible
-            if(!s) {
-                target.alpha = 0f
-                target.visibility = View.VISIBLE
+class ViewVisibilityAnimationChip<O>(private val target:O, private val startVisible:Boolean, private val endVisible:Boolean, private val gone:Boolean=false, private val maxAlpha:Float=1f)
+    : AbstractAnimChip() where O : View {
+    override fun process(r: Float) {
+        target.alpha = r * maxAlpha
+    }
+
+    override fun before(reverse: Boolean) {
+        val s = if(!reverse) startVisible else endVisible
+        if(!s) {
+            target.alpha = 0f
+            target.visibility = View.VISIBLE
+        }
+    }
+
+    override fun after(reverse: Boolean) {
+        val e = if(!reverse) endVisible else startVisible
+        if(!e) {
+            target.visibility = if(gone) View.GONE else View.INVISIBLE
+        }
+    }
+}
+
+class AnimSet(private val duration:Long) : AbstractAnimChip(), IAnimEngine {
+    private val list:MutableList<IAnimChip> = mutableListOf()
+    private var activeAnimator: ValueAnimator? = null
+    private var next:Boolean? = null
+
+    override fun cancel() {
+        activeAnimator?.run {
+            cancel()
+        }
+    }
+
+    override fun animate(reverse:Boolean, completed: ((Boolean) -> Unit)?) {
+        activeAnimator?.run {
+            cancel()
+            next = reverse
+            return
+        }
+
+        before(reverse)
+
+        activeAnimator = ValueAnimator.ofFloat(0f,1f).apply {
+            duration = this@AnimSet.duration
+            addUpdateListener {
+                process(it.animatedValue as Float)
             }
-        }
-
-        after = { reverse->
-            val e = if(!reverse) endVisible else startVisible
-            if(!e) {
-                target.visibility = if(gone) View.GONE else View.INVISIBLE
+            doOnEnd {
+                after(reverse)
+                activeAnimator = null
+                tryContinue(completed)
             }
+            doOnCancel {
+                activeAnimator = null
+                completed?.invoke(false)
+            }
+            start()
         }
     }
 
-    companion object {
-        fun bool2float(b: Boolean): Float {
-            return if (b) 1f else 0f
+    override fun process(r: Float) {
+        for(v in list) {
+            v.process(r)
         }
+    }
 
-        fun float2bool(f: Float): Boolean {
-            return if (f == 0f) false else true
+    override fun before(reverse: Boolean) {
+        for(v in list) {
+            v.before(reverse)
         }
+    }
+
+    override fun after(reverse: Boolean) {
+        for(v in list) {
+            v.after(reverse)
+        }
+    }
+    
+    private fun tryContinue(completed: ((Boolean) -> Unit)?) {
+        next?.let {
+            next = null
+            animate(it, completed)
+        } ?: completed?.invoke(true)
+    }
+
+    fun add(clip:IAnimChip) : AnimSet {
+        list.add(clip)
+        return this
     }
 }
 
-abstract class AnimDriver(du:Long) {
-    val animator:ValueAnimator = ValueAnimator.ofFloat(0f,1f).apply {
-        duration = du
-        addUpdateListener {
+class AnimSequence : IAnimEngine {
+    private val list = mutableListOf<IAnimEngine>()
+    private var currentEngine: IAnimEngine? = null
+    private var next:Boolean? = null
 
-        }
-    }
-    val list:MutableList<IAnimChip> = mutableListOf()
-
-    fun animate(reverse:Boolean) {
-        for(v in list) {
-            v.before?.invoke(reverse)
-        }
-
-        for(v in list) {
-            v.before?.invoke(reverse)
-        }
+    fun add(engine:IAnimEngine):AnimSequence {
+        list.add(engine)
+        return this
     }
 
 
-}
+    override fun cancel() {
+        currentEngine?.run {
+            cancel()
+        }
+    }
 
-class AnimSet : IAnimChip {
+    private suspend fun animateSub(engine:IAnimEngine, reverse:Boolean) = suspendCoroutine<Boolean> { cont->
+        currentEngine = engine
+        engine.animate(reverse) {
+            cont.resume(it)
+        }
+    }
+
+    override fun animate(reverse: Boolean, completed: ((Boolean) -> Unit)?) {
+        currentEngine?.run {
+            cancel()
+            next = reverse
+            return
+        }
+
+        val list: Iterable<IAnimEngine> = if (reverse) list.reverse() else list
+        MainScope().launch {
+            for (v in list) {
+                if(!animateSub(v, reverse)) {
+                    currentEngine = null
+                    return@launch
+                }
+            }
+            tryContinue(completed)
+        }
+    }
+
+    private fun tryContinue(completed: ((Boolean) -> Unit)?) {
+        next?.let {
+            next = null
+            animate(it, completed)
+        } ?: completed?.invoke(true)
+    }
 
 }
 
--- app/src/main/java/com/michael/ytremote/utils/Utils.kt
+++ app/src/main/java/com/michael/ytremote/utils/Utils.kt
@@ -1,3 +1,14 @@
 package com.michael.ytremote.utils
 
+import org.json.JSONArray
 
+
+fun <T> List<T>.reverse():Iterable<T> {
+    return Iterable {
+        iterator {
+            for (i in count()-1 downTo 0) {
+                yield(get(i)!!)
+            }
+        }
+    }
+}
